0ddb8a4163b417389fba30125598bc01c2a27b2f
Johnny Young <youngerblack@gmail.com>
2021-07-26 23:30:22 -0500

    Exercise 6
    Exercise 6 day 2

--- daytwo.js
+++ daytwo.js
@@ -1,2 +1,15 @@
 //Exercises día 2
 //Exercise 6
+let number = parseFloat(prompt("Please, enter a number to fin out if the number\n is greater than or less than or equal to 10: "));
+
+        let msg="";
+        if(number>10){
+            msg=`${number} is greater than 10`;
+        
+        } else if (number == 10){
+          msg=`${number} is equal than 10`
+        }
+        else{
+            msg=`${number} is less than 10 `
+        }
+        console.log(msg)
