4cb71ac3d0b4d42bc5e8814d808c5ffcd111704a
Johnny Young <youngerblack@gmail.com>
2021-07-28 00:22:29 -0500

    Adding validator TypeOf
    Adding validator TypeOf and index.html with 2 box and 2 button

--- daytwo.js
+++ daytwo.js
@@ -13,3 +13,24 @@ let number = parseFloat(prompt("Please, enter a number to fin out if the number\
             msg=`${number} is less than 10 `
         }
         console.log(msg)
+        console.log(`The number is: ${number} and the typeof is ${typeof(number)}`)
+
+        //Validator web
+        // const data1 = document.querySelector('#data1')
+        // const data2 = document.querySelector('#data2')
+        // const btn = document.querySelector('#btn')
+        
+
+        // btn.addEventListener('click', () =>{
+          
+        //   dato1 = data1.value
+        //   dato2 = data2.value
+        //   let result = dato1 + dato2
+
+        //   console.log(`The result is: ${result} and the typeof is ${typeof(result)}`)
+
+        // })
+
+
+
+//Exercise 7
