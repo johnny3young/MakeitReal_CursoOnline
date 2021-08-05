7d1c7d4a072cc13d5c9c39f4a397483ad30a438c
Johnny Young <youngerblack@gmail.com>
2021-08-05 00:14:02 -0500

    Dayfour
    Exercise 18

---
+++ dayfour.js
@@ -0,0 +1,12 @@
+//Exercise 18
+/* const names = ["Pedro", "Pablo", "María", "Juan", "Diana"]
+const insertFirstName = prompt(`Please type a name for add`)
+const insertSecondName = prompt(`Please type other name`)
+
+names.push(insertFirstName)
+names.splice(2,1,insertSecondName)
+//names[2] = insertSecondName
+
+console.log(names) */
+
+//Exercise 19
