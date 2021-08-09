f6dc7e81d5ef08d9754032f684911b4b075a376a
Johnny Young <youngerblack@gmail.com>
2021-08-08 21:25:28 -0500

    Exercise 22
    Finish exercises day four

--- dayfour.js
+++ dayfour.js
@@ -23,3 +23,35 @@ arrayNumber.splice(1,1)
 console.log(arrayNumber) */
 
 //Exercise 20
+/* const matriz = [
+    ["Pablo", "Maria", "Pedro"],
+    ["Diana", "Juan", "Federico"],
+    ["Roberto", "Daniel", "Sandra"]
+]
+
+for (i=0; i<matriz.length; i++){
+    console.log(`Grupo ${i+1}`)
+    for(k=0; k<matriz[i].length;k++){
+        console.log(`  ${matriz[i][k]}`)
+    }
+} */
+
+//Exercise 21
+/* const nums = [1, 23, 5, 6, 40, 12, 2, 67, 24, 9, 39]
+
+for(i=0; i<=nums.length; i++){
+    if(nums[i] > 10){
+      console.log(`${nums[i]}`)
+    }    
+}
+ */
+//Exercise 22
+const nums = [1, 1,,1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1]
+
+let totalOne = 0
+for (i=0; i<=nums.length;i++){  
+    if(nums[i] == 1){
+        totalOne ++        
+    }     
+}
+console.log(totalOne)
