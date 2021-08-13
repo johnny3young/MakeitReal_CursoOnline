2b38900d162ee60de1eadb9604c2aae33f4e9fff
Johnny Young <youngerblack@gmail.com>
2021-08-13 00:13:49 -0500

    Last exercise dayFive
    Last exercise dayFive

--- dayfive.js
+++ dayfive.js
@@ -24,9 +24,29 @@ phrase = phrase.replace(' ', '')
 console.log(phrase) */
 
 //Exercise 26
-let phrase = prompt("Please type a phrase:");
+/* let phrase = prompt("Please type a phrase:");
 phrase = phrase.trim().split(" ");
 console.log(phrase);
 phrase = phrase.map(p => p[0].toUpperCase() + p.slice(1));
 phrase = phrase.join(" ");
-console.log(phrase);
+console.log(phrase); */
+
+//Exercise 27
+/* const phrase = prompt("Please type a phrase")
+console.log(phrase)
+const phraseSeparated = phrase.split(' ')
+console.log(phraseSeparated)
+for(let i=0; i<phraseSeparated.length; i++){
+     console.log(phraseSeparated[i])
+} */
+
+//Exercise 28
+let phrase1 = prompt("Type a first phrase: ")
+let phrase2 = prompt("Type a second phrase: ")
+let comun = ""
+for(let i=0; i<phrase1.length; i++){
+  if (phrase2.includes(phrase1[i])) {
+  comun+=phrase1[i]+", "}
+}
+if (comun.length>=1) console.log(`Los caracteres en común son: ` + comun.slice(0, -2))
+else console.log("No se encontraron caracteres en común")
